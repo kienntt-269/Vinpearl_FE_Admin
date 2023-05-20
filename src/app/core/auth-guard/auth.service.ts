3c1ed64f05830f9e45a27fc6a03d28bd62a43ee7
Nguyễn Tiến Trung Kiên <99466207+kientee@users.noreply.github.com>
2023-05-20 22:18:44 +0700

    update vinpearl

--- src/app/core/auth-guard/auth.service.ts
+++ src/app/core/auth-guard/auth.service.ts
@@ -21,24 +21,6 @@ export class AuthService {
   isLoggedIn = false;
   redirectUrl!: string;
 
-  // <div *ngIf="authService.isLoggedIn">
-  //   Đây là trang được bảo vệ bởi đăng nhập
-  // </div>
-  // <div *ngIf="!authService.isLoggedIn">
-  //   Bạn cần đăng nhập để truy cập trang này
-  // </div>
-
-  // export class MyProtectedPageComponent {
-  //   constructor(private authService: AuthService, private router: Router) {}
-
-  //   ngOnInit() {
-  //     if (!this.authService.isLoggedIn) {
-  //       this.authService.redirectUrl = this.router.url;
-  //       this.router.navigate(['/login']);
-  //     }
-  //   }
-  // }
-
   login(email: any, password: any): Observable<any> {
     // return this.http.post(`${APIs.AUTH_LOGIN}`, {
       this.isLoggedIn = true;
